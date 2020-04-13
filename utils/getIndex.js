0bacecd72488945aa6866cd5dee293f4c5f3c6b8
sunyuting <sleepsun123@163.com>
2020-04-14 00:51:39 +0800

    fix

--- utils/getIndex.js
+++ utils/getIndex.js
@@ -40,7 +40,7 @@ let getClassify = async() => {
         'top_id': 0
       })
       .fetchAll({
-        columns: ['id', 'c_name', 'other']
+        columns: ['id', 'c_name']
       })
   const data = await makeSmall(bc)
   return data
@@ -49,23 +49,23 @@ let getClassify = async() => {
 const swiper = [
   {
     img: 'https://ae01.alicdn.com/kf/U9849b6d178b843bbada26bdec412f073g.jpg',
-    id: '222',
+    id: '33571',
     title: '2019科幻动作《终结者：黑暗命运》BD720P&BD1080P.英语中英双字'
   },{
     img: 'https://ae01.alicdn.com/kf/Uc9d893747c024afa960296414ef7476ev.jpg',
-    id: '222',
+    id: '33340',
     title: '2019高分韩剧《流浪者/浪客行》16集全.HD1080P.国韩双语中字'
   },{
     img: 'https://ae01.alicdn.com/kf/Uef16413b321244e4a99519450c8138d6A.jpg',
-    id: '222',
+    id: '40845',
     title: '2019科幻冒险《星球大战9：天行者崛起》BD720P&BD1080P.国英双语中英双字'
   },{
     img: 'https://ae01.alicdn.com/kf/U50a32afe33fe4ef8a727314dddc40f89Q.jpg',
-    id: '222',
+    id: '41959',
     title: '2020韩剧《王国第二季》6集全.HD1080P.韩语中字'
   },{
     img: 'https://ae01.alicdn.com/kf/U49e5e2811edb4d08a07b63b6ec6081fdG.jpg',
-    id: '222',
+    id: '30720',
     title: '2019爱情犯罪《少年的你》HD1080P.国语中字'
   }
 ];
@@ -105,7 +105,7 @@ const makeSmall = async(data) => {
       .orderBy('created_at', 'DESC')
       .fetchPage({
         limit: 9,
-        columns: ['id', 'title']
+        columns: ['id', 'title', 'other']
       })
       const mov = makeMovieArr(mo);
       cls.movie = mov;
